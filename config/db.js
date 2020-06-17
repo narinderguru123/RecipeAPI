cf162e8f0d8f2b8d2be3f265d69b9e67f5c840b3
narinderguru123 <narinderguru123@gmail.com>
2020-06-17 07:14:24 +0100

    first commit
    first commit

---
+++ config/db.js
@@ -0,0 +1,6 @@
+// creating module to handle db connection
+
+module.exports = {
+  //DB: "mongodb://localhost:27017/FoodAPI",
+  DB: "mongodb+srv://SunnyG:Blackm00n@cluster0-blgvp.gcp.mongodb.net/<dbname>?retryWrites=true&w=majority"
+};

48f826b6597bfdf2af5b2123590fa512e699addf
narinderguru123 <narinderguru123@gmail.com>
2020-06-18 07:09:49 +0100

    Update db.js

--- config/db.js
+++ config/db.js
@@ -2,5 +2,5 @@
 
 module.exports = {
   //DB: "mongodb://localhost:27017/FoodAPI",
-  DB: "mongodb+srv://SunnyG:Blackm00n@cluster0-blgvp.gcp.mongodb.net/<dbname>?retryWrites=true&w=majority"
+  DB:"mongodb+srv://SunnyG:Blackm00n@cluster0-blgvp.gcp.mongodb.net/FoodAPI?retryWrites=true&w=majority"
 };
